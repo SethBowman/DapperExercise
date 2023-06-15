f7d8a366f6ef6156a93fcdbdac66e2fc969e40bf
Seth Bowman <brandonsethbowman@gmail.com>
2023-06-15 09:36:30 -0500

    Exercise completed

---
+++ DapperExercise/IDepartmentRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperExercise
+{
+	public interface IDepartmentRepo
+	{
+		public IEnumerable<Department> GetAllDepartments();
+	}
+}
--- DapperExercise/Program.cs
+++ DapperExercise/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using DapperExercise;
+//^^^^MUST HAVE USING DIRECTIVES^^^^
+
+var config = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json")
+				.Build();
+
+string connString = config.GetConnectionString("DefaultConnection");
+
+IDbConnection conn = new MySqlConnection(connString);
+
+//var repo = new DepartmentRepo(conn);
+
+//Console.Write("Type a new Department name: ");
+
+//var newDepartment = Console.ReadLine();
+
+//repo.InsertDepartment(newDepartment);
+
+//var departments = repo.GetAllDepartments();
+
+//foreach(var department in departments)
+//{
+//    Console.WriteLine($"{department.Name} | {department.DepartmentID}");
+//}
+
+var repo = new DapperProductRepo(conn);
+
+//repo.CreateProduct("Diablo 4", 60.00, 1);
+//repo.DeleteProduct(942);
+//repo.UpdateProductName(943, "Banana");
+
+var products = repo.GetAllProducts();
+
+foreach(var product in products)
+{
+    Console.WriteLine($"{product.Name} | {product.Price} | {product.ProductID}");
+}
